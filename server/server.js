91f5d5bf054fe4b59eb3737c18d390813bdee1d2
yelo12324 <yeloindia.contact@gmail.com>
2025-08-02 13:28:53 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -54,7 +54,7 @@ app.post('/send-email', (req, res) => {
 });
 
 // 🚀 Start server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
